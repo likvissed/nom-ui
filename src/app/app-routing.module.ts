b111ee16d9d42b44792bc17d4eb7634cefc6c5be
Lika <bartuzanovaan@iss-reshetnev.ru>
2022-08-03 11:38:55 +0700

    feat(auth): Добавлена авторизация через ЛК

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { LayoutComponent } from './ui/layout/components/layout/layout.component'
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthCenterGuard } from '@iss/ng-auth-center';
+
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     component: LayoutComponent,
+    // canActivate: [AuthCenterGuard],
     children: [
       // routes
     ],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { PrimengModule } from './primeng.module';
 import { environment } from './../environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AuthCenterModule } from '@iss/ng-auth-center';
 
 
 @NgModule({
@@ -21,6 +24,11 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+
+    HttpClientModule,
+    AuthCenterModule.forRoot(environment.auth),
+    AppRoutingModule,
+
 
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
