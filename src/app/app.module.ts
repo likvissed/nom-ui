de4ce443426dd1ce9cb9c019bf8647d3425df880
Lika <bartuzanovaan@iss-reshetnev.ru>
2022-08-15 16:17:40 +0700

    feat(localize): Настройка локализации
    ru

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { ArticleModule } from './ui/article/shared/article.module';
 import { LayoutModule } from './ui/layout/shared/layout.module';
 import { PrimengModule } from './primeng.module';
 import { environment } from './../environments/environment';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,12 +12,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+
+import localeRu from '@angular/common/locales/ru';
 
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AuthCenterModule } from '@iss/ng-auth-center';
+
+registerLocaleData(localeRu);
 
 const appModules: any[] = [
   PrimengModule,
@@ -50,7 +55,12 @@ const appModules: any[] = [
 
     ...appModules
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'ru-RU',
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
